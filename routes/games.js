d685830c1600791b8a447d1ddd7a89c3e330d67d
minip <petrov-valera05@mail.ru>
2024-04-24 18:05:37 +0300

    routes changed

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,16 @@
 const gamesRouter = require("express").Router();
-const {sendAllGames, deleteGame, addGameController} = require("../controllers/games");
-const {getAllGames} = require("../middlewares/games");
+const {sendAllGames, sendUpdateGames} = require("../controllers/games");
+const {
+    getAllGames,
+    checkIsTitleInArray,
+    updateGamesArray,
+    updateGamesFile,
+    findGameById,
+    deleteGame
+} = require("../middlewares/games");
 
-gamesRouter.post("/games", getAllGames, addGameController);
 gamesRouter.get("/games", getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+gamesRouter.post("/games", getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdateGames);
+gamesRouter.delete("/games/:id", getAllGames, findGameById, deleteGame, updateGamesFile, sendUpdateGames);
 
 module.exports = gamesRouter;
